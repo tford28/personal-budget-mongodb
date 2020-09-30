dbf4c2a472c6e1f85809231801bc187827dabc88
Taylor Ford <tford28@uncc.edu>
2020-09-29 20:16:32 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var budget = require('./budget.json');
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
